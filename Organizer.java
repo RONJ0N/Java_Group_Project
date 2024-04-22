bc66d8008b7adaf41b91cd4bc54346ca7dd18f6a
RONJ0N <101667694+RONJ0N@users.noreply.github.com>
2024-04-22 14:17:03 -0400

    Update Organizer.java

--- Organizer.java
+++ Organizer.java
@@ -3,24 +3,21 @@ import java.util.HashMap;
 import java.util.HashSet;
 
 public class Organizer {
-    private String organizerName;
+   
     private ArrayList<Event> events;
     private HashMap<String, HashSet<String>> attendeeRSVPs;
-    private HashMap<String, HashMap<String, String>> eventFeedback;
+  
 
     public Organizer(String name) {
-        this.organizerName = name;
+ 
         this.events = new ArrayList<>();
         this.attendeeRSVPs = new HashMap<>();
-        this.eventFeedback = new HashMap<>();
     }
 
-    public void createEvent(String name, String date, String type, int max, ArrayList<String> speakers) {
-        if (type.equals("Conference")) {
-            events.add(new Conference(name, date, max, speakers));
-        } else {
-            // Handle other types of events
-        }
+    public void createEvent(String name, String date, ArrayList<String> speakers) {
+      
+            events.add(new Event(name, date));
+        
     }
 
     public void RSVP(String attendeeName, String eventName) {
@@ -30,12 +27,6 @@ public class Organizer {
         attendeeRSVPs.get(attendeeName).add(eventName);
     }
 
-    public void submitFeedback(String eventName, String attendeeName, String feedback) {
-        if (!eventFeedback.containsKey(eventName)) {
-            eventFeedback.put(eventName, new HashMap<>());
-        }
-        eventFeedback.get(eventName).put(attendeeName, feedback);
-    }
 
     public ArrayList<Event> getEvents() {
         return events;
